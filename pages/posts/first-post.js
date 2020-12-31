5311b5166e74fa2ae74ebf95af5c47edb15892cd
Cindy Le <cindyledev@gmail.com>
2020-12-30 22:52:28 -0500

    assets, metadata, css

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,21 @@
-import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+import styles from '../../styles/alert.module.css'
+import cn from 'classnames'
 
-export default function FirstPost() {
+export default function FirstPost({ children, type}) {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+      <div className={cn({
+        [styles.success]: type === 'success',
+        [styles.error]: type === 'error'
+      })}>
+        {children}
+      </div>
+    </Layout>
   )
 }
